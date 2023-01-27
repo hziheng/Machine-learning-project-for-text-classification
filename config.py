1ea664f5e62cb3906895b84348905a87e99b6134
hziheng <1034235826@qq.com>
2023-01-27 22:51:10 +0800

    first submit

---
+++ config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    : config.py
+@Time    : 2023/01/13 16:19:42
+@Author  : Huang zh
+@Contact : jacob.hzh@qq.com
+@Version : 0.1
+@Desc    : None
+'''
+
+ML_MODEL_NAME = ['lg', 'knn', 'dt', 'rt', 'gbdt', 'xgb', 'catboost', 'svm', 'bayes']
