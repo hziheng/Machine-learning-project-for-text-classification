6b0cef23a063d8b3e9209ed49de826d7b78f1f3c
hziheng <1034235826@qq.com>
2023-03-21 15:52:45 +0800

    add pretrain model

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ ML_MODEL_NAME = ['lg', 'knn', 'dt', 'rf', 'gbdt', 'xgb', 'catboost', 'svm', 'bay
 
 DL_MODEL_NAME = ['lstm', 'cnn', 'transformer', 'capsules']
 
-PRE_MODEL_NAME = ['mac_bert']
+PRE_MODEL_NAME = ['mac_bert', 'bert_wwm', 'bert']
 
 BATCH_SIZE = 8
 
